c7936681a112debd9c5949ae9797d7f6fa77a90f
OKUNOKENTARO <okunokentaro@me.com>
2016-05-26 05:16:41 +0900

    create a foundation of walts

---
+++ examples/walts/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ex-app',
+  template: `
+    <h1>Hello walts</h1>
+  `,
+})
+export class AppComponent {
+  constructor() {}
+}

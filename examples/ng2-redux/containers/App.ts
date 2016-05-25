e655ca36b9f23fd9ea14950f31e049c4e28e7bec
OKUNOKENTARO <okunokentaro@me.com>
2016-05-26 05:11:25 +0900

    create a foundation of ng2-redux

---
+++ examples/ng2-redux/containers/App.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ex-app',
+  template: `
+    <h1>Hello ng2-redux</h1>
+  `,
+})
+export class App {
+  constructor() {}
+}
---
+++ examples/ng2-redux/index.ts
@@ -0,0 +1,9 @@
+import 'core-js';
+import 'rxjs/Rx';
+import 'zone.js/dist/zone';
+
+import {bootstrap} from '@angular/platform-browser-dynamic';
+
+import {App} from './containers/App';
+
+bootstrap(App, []);

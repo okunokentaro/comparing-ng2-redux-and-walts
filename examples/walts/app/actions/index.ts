5ff4a39bf1a4012ac347d7ec028faaa159027397
OKUNOKENTARO <okunokentaro@me.com>
2016-08-26 23:14:23 +0900

    WIP [Not working] implement AppActions

--- examples/walts/app/actions/index.ts
+++ examples/walts/app/actions/index.ts
@@ -17,3 +17,95 @@ export const ACTIONS = [
   SetFilterAction,
   UpdateTodosAction
 ];
+
+import { Injectable } from '@angular/core';
+import { Actions, Action } from 'walts';
+
+import { AppState } from '../app.store';
+import { TodosRepository } from '../todos.repository';
+import { UpdateTodosAction } from './update-todos.action';
+
+@Injectable()
+export class AppActions extends Actions<AppState> {
+  constructor(private repository: TodosRepository,
+              private updateTodos: UpdateTodosAction) {
+    super();
+  }
+
+  AddTodo(text: string): Action<AppState>[] {
+    return this.combine(
+      (state) => {
+        this.repository.addTodo(text);
+        return state;
+      },
+      this.updateTodos.create()
+    );
+  }
+
+  clearCompletedAction(): Action<AppState>[] {
+    return this.combine(
+      (state) => {
+        this.repository.clearCompleted();
+        return state;
+      },
+      this.updateTodos.create()
+    );
+  }
+
+  completeAll(): Action<AppState>[] {
+    return this.combine(
+      (state) => {
+        this.repository.completeAll();
+        return state;
+      },
+      this.updateTodos.create()
+    );
+  }
+
+  completeTodo(id: number): Action<AppState>[] {
+    return this.combine(
+      (state) => {
+        this.repository.completeTodo(id);
+        return state;
+      },
+      this.updateTodos.create()
+    );
+  }
+
+  deleteTodo(id: number): Action<AppState>[] {
+    return this.combine(
+      (state) => {
+        this.repository.deleteTodo(id);
+        return state;
+      },
+      this.updateTodos.create()
+    );
+  }
+
+  editTodo(id: number, text: string): Action<AppState>[] {
+    return this.combine(
+      (state) => {
+        this.repository.editTodo(id, text);
+        return state;
+      },
+      this.updateTodos.create()
+    );
+  }
+
+  setFilter(filter: FilterType): Action<AppState>[] {
+    return this.combine(
+      (state) => ({
+        filter
+      }),
+      this.updateTodos.create()
+    )
+  }
+
+  updateTodos(): Action<AppState> {
+    return (state) => ({
+      todos         : this.repository.filterByType(state.filter),
+      completedCount: this.repository.completedCount(),
+      activeCount   : this.repository.activeCount()
+    });
+  }
+}
